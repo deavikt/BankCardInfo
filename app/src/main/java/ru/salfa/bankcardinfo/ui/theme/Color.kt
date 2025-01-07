eaede2083d962071f9b4913e3e3c90b143ad9b8d
Diana V <deavikt@mail.ru>
2025-01-07 21:40:13 +0400

    add bin info card

--- app/src/main/java/ru/salfa/bankcardinfo/ui/theme/Color.kt
+++ app/src/main/java/ru/salfa/bankcardinfo/ui/theme/Color.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.graphics.Color
 val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
 val Purple = Color(0xFF9A10F0)
+val Red = Color(0xFFF0114C)
 
 val Hint = Color(0xFFADB5BD)
 val DisabledContainer = Color(0xFFF6F6FA)
